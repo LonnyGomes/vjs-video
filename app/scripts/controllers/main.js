cf5574fe29e9e78f4a6242b050be6d8a778e96e3
Lonny Gomes <code@lonnygomes.com>
2015-08-15 14:26:56 -0400

    Added generated files from yeoman angular genrator

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vjsVideoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vjsVideoApp
+ */
+angular.module('vjsVideoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
