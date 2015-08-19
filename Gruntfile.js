0e21c26603252049b3715db2c36c0edd6189884f
Lonny Gomes <code@lonnygomes.com>
2015-08-18 21:07:07 -0400

    Removed image min from grunt and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -277,17 +277,6 @@ module.exports = function (grunt) {
     //   dist: {}
     // },
 
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/images'
-        }]
-      }
-    },
-
     svgmin: {
       dist: {
         files: [{
@@ -388,9 +377,7 @@ module.exports = function (grunt) {
         'copy:styles'
       ],
       dist: [
-        'copy:styles',
-        'imagemin',
-        'svgmin'
+        'copy:styles'
       ]
     },
 
