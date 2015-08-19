cb4f645011f98ac25cb2028f95b68dc370a5d4a3
Lonny Gomes <code@lonnygomes.com>
2015-08-18 22:48:20 -0400

    Created basic implementation of videojs directive

--- app/scripts/directives/vjs.directive.js.js
+++ app/scripts/directives/vjs.directive.js.js
@@ -1,4 +1,4 @@
-'use strict';
+/*global angular */
 
 /**
  * @ngdoc directive
@@ -6,13 +6,43 @@
  * @description
  * # vjs.directive.js
  */
-angular.module('vjsVideoApp')
-  .directive('vjs.directive.js', function () {
+angular.module('vjs.video', [])
+  .directive('vjsVideo', function () {
+    'use strict';
+
     return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the vjs.directive.js directive');
+      restrict: 'A',
+      transclude: true,
+      link:  function postLink(scope, element, attrs, ctrl, transclude) {
+        var vid = null;
+        if (element[0].nodeName === 'VIDEO') {
+          vid = element[0];
+        }
+
+        if (vid) {
+          if (window.videojs) {
+            //attach transcluded content
+            transclude(function (content) {
+              element.append(content);
+            });
+
+            //bootstrap videojs
+            window.videojs(vid, {
+              //options
+            }, function () {
+
+            });
+
+            //dispose of videojs before destroying directive
+            scope.$on('$destroy', function () {
+              window.videojs(vid).dispose();
+            });
+          } else {
+            throw new Error('video.js was not found!');
+          }
+        } else {
+          throw new Error('directive must be attached to a video tag!');
+        }
       }
     };
   });
