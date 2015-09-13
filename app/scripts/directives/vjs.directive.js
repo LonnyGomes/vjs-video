e32b4193b0c7e0a4c0b9b73003466ad9ce766cc7
Lonny Gomes <code@lonnygomes.com>
2015-09-13 18:16:05 -0400

    Started work on creating a basic responsive container directive - #1

--- app/scripts/directives/vjs.directive.js
+++ app/scripts/directives/vjs.directive.js
@@ -11,16 +11,30 @@
 
     var module = angular.module('vjs.video', []);
 
-    function getVidElement(element) {
-        var vid = null;
+    function getVidElement(element, isContainer) {
+        var vid = null,
+            videos;
+
         if (!window.videojs) {
             throw new Error('video.js was not found!');
         }
 
-        if (element[0].nodeName === 'VIDEO') {
-            vid = element[0];
+        if (isContainer) {
+            videos = element.getElementsByTagName("video");
+            if (videos.length === 0) {
+                throw new Error('video tag must be defined within container directive!');
+            } else if (videos.length > 1) {
+                throw new Error('only one video can be defined within the container directive!');
+            }
+
+            vid = videos[0];
+
         } else {
-            throw new Error('directive must be attached to a video tag!');
+            if (element[0].nodeName === 'VIDEO') {
+                vid = element[0];
+            } else {
+                throw new Error('directive must be attached to a video tag!');
+            }
         }
 
         return vid;
@@ -58,6 +72,22 @@
                 transclude(function (content) {
                     element.append(content);
                 });
+
+                initVideoJs(vid, scope, scope.vjsSetup);
+            }
+        };
+    });
+
+    module.directive('vjsVideoContainer', function () {
+        return {
+            restrict: 'AE',
+            transclude: true,
+            templateUrl: 'scripts/directives/vjs.container.html',
+            scope: {
+                vjsSetup: '='
+            },
+            link: function postLink(scope, element, attrs, ctrl) {
+                var vid = getVidElement(element, true);
 
                 initVideoJs(vid, scope, scope.vjsSetup);
             }
