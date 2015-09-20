7c528c8a89342cfeb4f705e6457187a922195b8d
Lonny Gomes <code@lonnygomes.com>
2015-09-20 02:16:54 -0400

    Moved most functionality for directives into a shared controller - #1
    The idea is to to make things a bit more testable than they are now.
    Currently unit tests are failing and the hope is this strategy is more testable

--- app/scripts/directives/vjs.directive.js
+++ app/scripts/directives/vjs.directive.js
@@ -11,132 +11,140 @@
 
     var module = angular.module('vjs.video', []);
 
-    function getVidElement(element, isContainer) {
-        var vid = null,
-            videos;
+    module.controller('VjsVideoController', ['$scope', function ($scope) {
+        var self = this;
 
-        if (!window.videojs) {
-            throw new Error('video.js was not found!');
-        }
+        function getVidElement(element, isContainer) {
+            var vid = null,
+                videos;
 
-        if (isContainer) {
-            videos = element[0].getElementsByTagName('video');
-            if (videos.length === 0) {
-                throw new Error('video tag must be defined within container directive!');
-            } else if (videos.length > 1) {
-                throw new Error('only one video can be defined within the container directive!');
+            if (!window.videojs) {
+                throw new Error('video.js was not found!');
             }
 
-            vid = videos[0];
-        } else {
-            if (element[0].nodeName === 'VIDEO') {
-                vid = element[0];
+            if (isContainer) {
+                videos = element[0].getElementsByTagName('video');
+                if (videos.length === 0) {
+                    throw new Error('video tag must be defined within container directive!');
+                } else if (videos.length > 1) {
+                    throw new Error('only one video can be defined within the container directive!');
+                }
+
+                vid = videos[0];
             } else {
-                throw new Error('directive must be attached to a video tag!');
-            }
-        }
-
-        return vid;
-    }
-
-    function applyRatio(el, ratioVal) {
-        var ratio = ratioVal,
-            style = document.createElement('style'),
-            parseRatio = function (r) {
-                var tokens = r.split('/'),
-                    tokenErrorMsg = 'the ratio must either be "wide", "standard" or ' +
-                                    'decimal values in the format of w/h';
-
-                //if invalid ratio throw an error
-                if (tokens.length !== 2) {
-                    throw new Error(tokenErrorMsg);
-                }
-
-                //confirm that both tokens are numbers
-                if (isNaN(tokens[0]) || isNaN(tokens[1])) {
-                    throw new Error(tokenErrorMsg);
-                }
-
-                //confirm that the width or height is not zero
-                if (Number(tokens[0]) === 0 || Number(tokens[1]) === 0) {
-                    throw new Error('neither the width or height ratio can be zero!');
-                }
-
-                return (Number(tokens[1]) / Number(tokens[0])) * 100;
-            },
-            genContainerId = function (element) {
-                var container = element[0].querySelector('.video-js'),
-                    vjsId;
-
-                if (container) {
-                    vjsId = 'vjs-container-' + container.getAttribute('id');
+                if (element[0].nodeName === 'VIDEO') {
+                    vid = element[0];
                 } else {
-                    //vjsId = 'vjs-container-default';
-                    throw new Error('Failed to find instance of video-js class!');
+                    throw new Error('directive must be attached to a video tag!');
                 }
-
-                //add generated id to container
-                element[0].setAttribute('id', vjsId);
-
-                return vjsId;
-            },
-            containerId,
-            ratioPercentage,
-            css;
-
-        //if ratio isn't defined lets default to wide screen
-        if (!ratio) {
-            ratio = '16/9';
-        }
-
-        switch (ratio) {
-        case 'wide':
-            ratio = '16/9';
-            break;
-        case 'standard':
-            ratio = '4/3';
-            break;
-        }
-
-        containerId = genContainerId(el);
-
-        ratioPercentage = parseRatio(ratio);
-
-        css = ['#', containerId, ' ',
-               '.video-js {padding-top:', ratioPercentage,
-               '%;}\n', '.vjs-fullscreen {padding-top: 0px;}'].join('');
-
-        style.type = 'text/css';
-        style.rel = 'stylesheet';
-        if (style.styleSheet) {
-            style.styleSheet.cssText = css;
-        } else {
-            style.appendChild(document.createTextNode(css));
-        }
-
-        el[0].appendChild(style);
-    }
-
-    function initVideoJs(vid, scope, element) {
-        var opts = scope.vjsSetup || {},
-            ratio = scope.vjsRatio;
-
-        if (!window.videojs) {
-            return null;
-        }
-
-        //bootstrap videojs
-        window.videojs(vid, opts, function () {
-            if (element[0].nodeName !== 'VIDEO') {
-                applyRatio(element, ratio);
             }
-        });
 
-        //dispose of videojs before destroying directive
-        scope.$on('$destroy', function () {
-            window.videojs(vid).dispose();
-        });
-    }
+            return vid;
+        }
+
+        function applyRatio(el, ratioVal) {
+            var ratio = ratioVal,
+                style = document.createElement('style'),
+                parseRatio = function (r) {
+                    var tokens = r.split('/'),
+                        tokenErrorMsg = 'the ratio must either be "wide", "standard" or ' +
+                                        'decimal values in the format of w/h';
+
+                    //if invalid ratio throw an error
+                    if (tokens.length !== 2) {
+                        throw new Error(tokenErrorMsg);
+                    }
+
+                    //confirm that both tokens are numbers
+                    if (isNaN(tokens[0]) || isNaN(tokens[1])) {
+                        throw new Error(tokenErrorMsg);
+                    }
+
+                    //confirm that the width or height is not zero
+                    if (Number(tokens[0]) === 0 || Number(tokens[1]) === 0) {
+                        throw new Error('neither the width or height ratio can be zero!');
+                    }
+
+                    return (Number(tokens[1]) / Number(tokens[0])) * 100;
+                },
+                genContainerId = function (element) {
+                    var container = element[0].querySelector('.video-js'),
+                        vjsId;
+
+                    if (container) {
+                        vjsId = 'vjs-container-' + container.getAttribute('id');
+                    } else {
+                        //vjsId = 'vjs-container-default';
+                        throw new Error('Failed to find instance of video-js class!');
+                    }
+
+                    //add generated id to container
+                    element[0].setAttribute('id', vjsId);
+
+                    return vjsId;
+                },
+                containerId,
+                ratioPercentage,
+                css;
+
+            //if ratio isn't defined lets default to wide screen
+            if (!ratio) {
+                ratio = '16/9';
+            }
+
+            switch (ratio) {
+            case 'wide':
+                ratio = '16/9';
+                break;
+            case 'standard':
+                ratio = '4/3';
+                break;
+            }
+
+            containerId = genContainerId(el);
+
+            ratioPercentage = parseRatio(ratio);
+
+            css = ['#', containerId, ' ',
+                   '.video-js {padding-top:', ratioPercentage,
+                   '%;}\n', '.vjs-fullscreen {padding-top: 0px;}'].join('');
+
+            style.type = 'text/css';
+            style.rel = 'stylesheet';
+            if (style.styleSheet) {
+                style.styleSheet.cssText = css;
+            } else {
+                style.appendChild(document.createTextNode(css));
+            }
+
+            el[0].appendChild(style);
+        }
+
+        function initVideoJs(vid, params, element) {
+            var opts = params.vjsSetup || {},
+                ratio = params.vjsRatio;
+
+            if (!window.videojs) {
+                return null;
+            }
+
+            //bootstrap videojs
+            window.videojs(vid, opts, function () {
+                if (element[0].nodeName !== 'VIDEO') {
+                    applyRatio(element, ratio);
+                }
+            });
+
+            //dispose of videojs before destroying directive
+            $scope.$on('$destroy', function () {
+                window.videojs(vid).dispose();
+            });
+        }
+
+        //export public methods
+        self.initVideoJs = initVideoJs;
+        self.getVidElement = getVidElement;
+    }]);
 
     module.directive('vjsVideo', function () {
         return {
@@ -145,15 +153,22 @@
             scope: {
                 vjsSetup: '='
             },
+            controller: 'VjsVideoController',
+            controllerAs: 'vjsCtrl',
+            bindToController: true,
             link: function postLink(scope, element, attrs, ctrl, transclude) {
-                var vid = getVidElement(element);
+                var vid = ctrl.getVidElement(element),
+                    params = {
+                        vjsSetup: ctrl.vjsSetup,
+                        vjsRatio: ctrl.vjsRatio
+                    };
 
                 //attach transcluded content
                 transclude(function (content) {
                     element.append(content);
                 });
 
-                initVideoJs(vid, scope, element);
+                ctrl.initVideoJs(vid, params, element);
             }
         };
     });
@@ -167,16 +182,22 @@
                 vjsSetup: '=',
                 vjsRatio: '@'
             },
+            controller: 'VjsVideoController',
+            controllerAs: 'vjsCtrl',
+            bindToController: true,
             link: function postLink(scope, element, attrs, ctrl) {
-                var vid = getVidElement(element, true),
-                    ratio = scope.vjsRatio;
+                var vid = ctrl.getVidElement(element, true),
+                    params = {
+                        vjsSetup: ctrl.vjsSetup,
+                        vjsRatio: ctrl.vjsRatio
+                    };
 
                 //set width and height of video to auto
                 vid.setAttribute('width', 'auto');
                 vid.setAttribute('height', 'auto');
 
                 //bootstrap video js
-                initVideoJs(vid, scope, element);
+                ctrl.initVideoJs(vid, params, element);
 
                 //apply ratio to element
                 //applyRatio(element, ratio);
