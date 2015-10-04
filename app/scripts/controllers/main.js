cd7801fe4e62b559e62c902f2bbecc015ce46e07
Lonny Gomes <code@lonnygomes.com>
2015-10-04 01:29:25 -0400

    Updated test page to include mulptile sources and a toggle button - #5

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,29 +9,53 @@
 angular.module('vjsVideoApp')
     .controller('MainCtrl', ['$scope', function (scope) {
         'use strict';
+
+        var mediaObj = {
+                sources: [
+                    {
+                        src: 'http://video-js.zencoder.com/oceans-clip.mp4',
+                        type: 'video/mp4'
+                    },
+                    {
+                        src: 'http://video-js.zencoder.com/oceans-clip.webm',
+                        type: 'video/webm'
+                    }
+                ],
+                tracks: []
+            },
+            mediaAltObj = {
+                sources: [
+                    {
+                        src: 'http://html5videoformatconverter.com/data/images/happyfit2.mp4',
+                        type: 'video/mp4'
+                    },
+                    {
+                        src: 'http://html5videoformatconverter.com/data/images/happyfit2.webm',
+                        type: 'video/webm'
+                    }
+                ],
+                tracks: []
+            },
+            isToggled = false;
+
         this.awesomeThings = [
             'HTML5 Boilerplate',
             'AngularJS',
             'Karma'
         ];
 
+        scope.toggleMedia = function () {
+            isToggled = !isToggled;
+            scope.mediaToggle = (isToggled) ? mediaObj : mediaAltObj;
+        };
+
         scope.options = {
             loop: true
         };
 
-        scope.media = {
-            sources: [
-                {
-                    src: 'http://video-js.zencoder.com/oceans-clip.mp4',
-                    type: 'video/mp4'
-                },
-                {
-                    src: 'http://video-js.zencoder.com/oceans-clip.webm',
-                    type: 'video/webm'
-                }
-            ],
-            tracks: []
-        };
+        scope.media = mediaObj;
+
+        scope.mediaToggle = mediaAltObj;
 
         scope.$on('vjsVideoReady', function (e, data) {
             //data contains `id` and `vid`
