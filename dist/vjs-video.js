cb08cb9b0ea6831df71a0954662e7634edc9ffd1
Lonny Gomes <code@lonnygomes.com>
2015-12-01 22:39:19 -0500

    Commiting dist build for v0.1.3

--- dist/vjs-video.js
+++ dist/vjs-video.js
@@ -235,7 +235,8 @@
                 //emit ready event with reference to video
                 $scope.$emit('vjsVideoReady', {
                     id: vid.getAttribute('id'),
-                    vid: this
+                    vid: this,
+                    controlBar: this.controlBar
                 });
             });
 
@@ -256,8 +257,8 @@
             restrict: 'A',
             transclude: true,
             scope: {
-                vjsSetup: '=',
-                vjsMedia: '='
+                vjsSetup: '=?',
+                vjsMedia: '=?'
             },
             controller: 'VjsVideoController',
             controllerAs: 'vjsCtrl',
@@ -328,9 +329,9 @@
             transclude: true,
             template: '<div class="vjs-directive-container"><div ng-transclude></div></div>',
             scope: {
-                vjsSetup: '=',
+                vjsSetup: '=?',
                 vjsRatio: '@',
-                vjsMedia: '='
+                vjsMedia: '=?'
             },
             controller: 'VjsVideoController',
             controllerAs: 'vjsCtrl',
