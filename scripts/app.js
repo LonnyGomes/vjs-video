a63df6f5239d948465879bcfd3a042084613ba72
Lonny Gomes <code@lonnygomes.com>
2016-06-25 19:08:26 -0400

    deployment build

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vjsVideoApp
+ * @description
+ * # vjsVideoApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('vjsVideoApp', [
+        'vjs.video',
+        'gist'
+    ]);
